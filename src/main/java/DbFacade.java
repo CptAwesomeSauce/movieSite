c6758765a9d5fe20d824b50fe8b01ad48eca09a6
stoppede <stoppede@plu.edu>
2018-05-07 15:50:26 -0700

    learn git

--- src/main/java/DbFacade.java
+++ src/main/java/DbFacade.java
@@ -14,6 +14,7 @@ public class DbFacade implements AutoCloseable {
     private String username;
     private String password;
 
+    // I learned GIT [DAVE - (253 -778 -0895)
     public DbFacade() {
         System.out.println("this is working and building as desired");
         //open connection to DB
